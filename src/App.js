e6efe9e63607ef02325d00ecc4402af8136359ec
jorgeeder <jorgeeder.js@gmail.com>
2019-10-17 08:39:51 -0300

    Final

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 
 import Header from './components/Header';
 import PostList from './components/PostList';
+import PostItem from './components/PostItem';
 
 function App() {
   return (
     <>
       <Header />
+      <PostList/>
     </>
   );
 }
